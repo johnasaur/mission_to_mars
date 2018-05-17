8b43450f33e5ae3e929a1bdc8619adcd51e3796e
johnasaur <33011932+johnasaur@users.noreply.github.com>
2018-05-16 23:03:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,15 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
+    print(mars)
     return render_template("index.html", mars = mars)
 
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars 
-    facts_m = scrape_mars_new.scrape()
+    facts_m = scrape_mars_new.scrape
     mars.update({}, facts_m, upsert=True)
+    print(facts_m)
     return redirect("http://localhost:5000/", code=302)
 
 if __name__ == "__main__":
